bcef46fcf770649f6025edaca9c7bac93b1d9246
Ivan <isharapov@bk.ru>
2021-03-25 09:26:18 +0000

    Adding files for deploy to server

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -8,9 +8,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '*',
-    '127.0.0.1',
-    'localhost',
-    '[::1]',
 ]
 
 INSTALLED_APPS = [
@@ -64,8 +61,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+	'USER': os.environ.get('POSTGRES_USER'),
+	'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+	'HOST': os.environ.get('DB_HOST'),
+	'PORT': os.environ.get('DB_PORT'),
     }
 }
 
